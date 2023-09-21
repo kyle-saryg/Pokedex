20bf2b19f40ec2e0a17ba9a1e836cc8129e62fed
kyle-saryg <kyle.cuyos13@gmail.com>
2023-09-21 16:29:41 -0700

    Initial Commit

---
+++ Pokedex/Pokedex/PokedexApp.swift
@@ -0,0 +1,17 @@
+//
+//  PokedexApp.swift
+//  Pokedex
+//
+//  Created by Kyle Sarygin on 9/21/23.
+//
+
+import SwiftUI
+
+@main
+struct PokedexApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
